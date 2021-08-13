bce1703fc5fb41cd0a8222bce71666df4cd0ee50
Bastien Boutonnet <bastien.b1@gmail.com>
2021-08-13 17:20:12 +0200

    start implementing report task with authentication

--- status_villain/commands.py
+++ status_villain/commands.py
@@ -18,7 +18,7 @@ def hash_password(password):
     return (salt + pwdhash).decode("ascii")
 
 
-def verify_password(stored_password, provided_password):
+def check_password(stored_password, provided_password):
     """Verify a stored password against one provided by user"""
     salt = stored_password[:64]
     stored_password = stored_password[64:]
--- status_villain/tasks/tasks.py
+++ status_villain/tasks/tasks.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from pathlib import Path
 from typing import Dict
 
@@ -6,11 +7,14 @@ import yaml
 from pydantic import BaseModel
 from rich.console import Console
 
-from status_villain.commands import create_user
+from status_villain.commands import check_password, create_user
+from status_villain.database import database_connector
+from status_villain.models import User
 
 console = Console()
 
 DEFAULT_PROFILES_DIR = Path("~/.status_villain/").expanduser().resolve()
+DEFAULT_PROFILES_FILE = DEFAULT_PROFILES_DIR.joinpath("credentials.yaml")
 
 
 def make_dir(path: Path):
@@ -45,7 +49,18 @@ class UserInfoInputModel(BaseModel):
     email: str
 
 
-class InitTask:
+class InteractiveLoginInputModel(BaseModel):
+    email: str
+    username: str
+    password: str
+
+
+class BaseTask(ABC):
+    def run(self):
+        ...
+
+
+class InitTask(BaseTask):
     def __init__(self):
         self.username: str
         self.first_name: str
@@ -58,18 +73,17 @@ class InitTask:
             make_dir(DEFAULT_PROFILES_DIR)
 
     def create_profiles_file(self):
-        credentials_file_path = DEFAULT_PROFILES_DIR.joinpath("credentials.yaml")
-        if not credentials_file_path.exists():
-            make_file(credentials_file_path, self.user_info.dict())
+        if not DEFAULT_PROFILES_FILE.exists():
+            make_file(DEFAULT_PROFILES_FILE, self.user_info.dict())
         else:
             response = questionary.confirm(
-                "A credentials file already exists at {credentials_file_path}. Do you want to overwrite it?"
+                f"A credentials file already exists at {DEFAULT_PROFILES_FILE}. Do you want to overwrite it?"
             ).ask()
             if response is True:
-                make_file(credentials_file_path, self.user_info.dict())
+                make_file(DEFAULT_PROFILES_FILE, self.user_info.dict())
             else:
                 raise CredentialsFilesAlreadyPopulated(
-                    f"A credentials file already exists at {credentials_file_path}. "
+                    f"A credentials file already exists at {DEFAULT_PROFILES_FILE}. "
                     "Please update it with your new credentials if they have changed"
                 )
 
@@ -104,3 +118,63 @@ class InitTask:
         create_user(self.email, self.username, self.first_name, self.last_name, self.password)
         self.persist_credentials()
         console.print("You're all set")
+
+
+class ReportTask(BaseTask):
+    def __init__(self) -> None:
+        self.is_authed_user: bool
+
+    def attempt_login(self):
+        _credentials = {}
+        if DEFAULT_PROFILES_FILE.exists():
+            with open(DEFAULT_PROFILES_FILE, "r") as file:
+                _credentials = yaml.safe_load(file)
+                self.credentials = UserInfoInputModel(**_credentials)
+        else:
+            login_prompts = [
+                {"type": "text", "name": "email", "message": "e-mail address"},
+                {"type": "text", "name": "username", "message": "Username"},
+                {"type": "password", "name": "password", "message": "Password"},
+            ]
+            user_info = questionary.prompt(login_prompts)
+            self.credentials = InteractiveLoginInputModel(**user_info)
+
+        if self.credentials:
+            self.is_authed_user = self.authenticate()
+            if self.is_authed_user is False:
+                console.print("Could not authenticate, check your credentials")
+        else:
+            return
+
+    def authenticate(self) -> bool:
+        with database_connector.session_manager() as session:
+            user_password = (
+                session.query(User.password)
+                .filter(
+                    User.email == self.credentials.email, User.username == self.credentials.username
+                )
+                .first()
+            )
+            if user_password:
+                stored_password = user_password[0]
+                is_valid_user = check_password(stored_password, self.credentials.password)
+                return is_valid_user
+            else:
+                return False
+
+    def run(self):
+        self.attempt_login()
+        # we will need to check that there are credentials or ask for user to log in.
+
+        # get the potential previous status report for this user.
+
+        # if we have it display it in a nice fashion using `rich` markdown rendering capabilities
+
+        # did you complete all your tasks from yesterday
+        #    # if so, we will set a "completed" boolean --which is going to be nice to display streaks
+        #    # and we will pre-fill yesterdays message for "today" with the content of yeterday.
+        #    # if not, we pop a txt editor and allow the user to write an amended version of his yesterday status.
+
+        # pop an editor again to ask for TODAY's update.
+
+        # persist reports to the messages tables.
